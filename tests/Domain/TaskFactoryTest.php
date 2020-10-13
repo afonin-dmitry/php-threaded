af556300a85bc4b9edd3f91877880920aaebe498
dmitry <dmitry.iup@yandex.ru>
2020-10-14 01:32:23 +0300

    replace tabs with spaces

--- tests/Domain/TaskFactoryTest.php
+++ tests/Domain/TaskFactoryTest.php
@@ -10,29 +10,29 @@ use PHPUnit\Framework\TestCase;
 
 class TaskFactoryTest extends TestCase
 {
-	public function testTasksTotalCount(): void
-	{
-		/** @var Task[] $tasks */
-		$tasks = [];
-		$factory = new TaskGenerator(10, 1000, 10);
+    public function testTasksTotalCount(): void
+    {
+        /** @var Task[] $tasks */
+        $tasks = [];
+        $factory = new TaskGenerator(10, 1000, 10);
 
-		foreach($factory->batch() as $batch) {
-			$tasks = array_merge($tasks, $batch);
-		}
+        foreach($factory->batch() as $batch) {
+            $tasks = array_merge($tasks, $batch);
+        }
 
-		$this->assertCount(1000, $tasks);
-	}
+        $this->assertCount(1000, $tasks);
+    }
 
-	public function testTasksOrder(): void
-	{
-		$factory = new TaskGenerator(100, 1000, 10);
+    public function testTasksOrder(): void
+    {
+        $factory = new TaskGenerator(100, 1000, 10);
 
-		$sorted_tasks = $tasks = $factory->batch()->current();
-		usort($sorted_tasks, function(Task $a, Task $b) {
-			return $a->getId() <=> $b->getId();
-		});
+        $sorted_tasks = $tasks = $factory->batch()->current();
+        usort($sorted_tasks, function(Task $a, Task $b) {
+            return $a->getId() <=> $b->getId();
+        });
 
-		$this->assertNotCount(0, $tasks);
-		$this->assertEquals($tasks, $sorted_tasks);
-	}
+        $this->assertNotCount(0, $tasks);
+        $this->assertEquals($tasks, $sorted_tasks);
+    }
 }
