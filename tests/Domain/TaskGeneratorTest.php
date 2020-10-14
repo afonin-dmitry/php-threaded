fe03096728a76721a077594f8b3a93bf3ca96047
dmitry <dmitry.iup@yandex.ru>
2020-10-14 10:41:51 +0300

    Test and variable renamed

--- tests/Domain/TaskGeneratorTest.php
+++ tests/Domain/TaskGeneratorTest.php
@@ -8,15 +8,15 @@ use App\Domain\Task;
 use App\Domain\TaskGenerator;
 use PHPUnit\Framework\TestCase;
 
-class TaskFactoryTest extends TestCase
+class TaskGeneratorTest extends TestCase
 {
     public function testTasksTotalCount(): void
     {
         /** @var Task[] $tasks */
         $tasks = [];
-        $factory = new TaskGenerator(10, 1000, 10);
+        $generator = new TaskGenerator(10, 1000, 10);
 
-        foreach($factory->batch() as $batch) {
+        foreach($generator->batch() as $batch) {
             $tasks = array_merge($tasks, $batch);
         }
 
@@ -25,9 +25,9 @@ class TaskFactoryTest extends TestCase
 
     public function testTasksOrder(): void
     {
-        $factory = new TaskGenerator(100, 1000, 10);
+        $generator = new TaskGenerator(100, 1000, 10);
 
-        $sorted_tasks = $tasks = $factory->batch()->current();
+        $sorted_tasks = $tasks = $generator->batch()->current();
         usort($sorted_tasks, function(Task $a, Task $b) {
             return $a->getId() <=> $b->getId();
         });
